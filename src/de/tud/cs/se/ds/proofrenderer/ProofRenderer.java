e8bb0e48c299175c765998382ef8b9c869f45d23
Dominic Scheurer <scheurer@cs.tu-darmstadt.de>
2016-05-19 11:35:17 +0200

    Properly included antlr

--- src/de/tud/cs/se/ds/proofrenderer/ProofRenderer.java
+++ src/de/tud/cs/se/ds/proofrenderer/ProofRenderer.java
@@ -4,10 +4,7 @@
 package de.tud.cs.se.ds.proofrenderer;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-
-import de.tud.cs.se.ds.proofrenderer.model.ProofTree;
 
 /**
  * TODO
@@ -15,9 +12,6 @@ import de.tud.cs.se.ds.proofrenderer.model.ProofTree;
  * @author Dominic Scheurer
  */
 public class ProofRenderer {
-    private static enum Scopes {
-        TOP_LEVEL, COMMENT, STRING, OP_EXPECTED, OP_DEF
-    }
     
     private File proofTreeFile = null; 
 
@@ -69,61 +63,6 @@ public class ProofRenderer {
     }
 
     public String render() throws IOException {
-        final ProofTree parsed = parse();
-        return parsed.toString();
-    }
-
-    private ProofTree parse() throws IOException {
-        final ProofTree result = new ProofTree();
-        
-        int lineNo = 0, indent = 0;
-        Scopes scope = Scopes.TOP_LEVEL;
-        Scopes scopeBefore = scope;
-        boolean skipWhiteSpace = false;
-        boolean readUntilWhiteSpace = false;
-        
-        FileReader rd = new FileReader(proofTreeFile);
-        
-        StringBuilder bf = null;
-        char c;
-        while ((c = (char) rd.read()) != (char) -1) {
-            
-            if (skipWhiteSpace) {
-                if (c == ' ' || c == '\t' || c == '\n') {
-                    continue;
-                }
-            }
-            
-            if (scope == Scopes.COMMENT) {
-                if (c == '\n') {
-                    scope = scopeBefore;
-                    continue;
-                } else {
-                    continue;
-                }
-            }
-            
-
-            if (c == ';' && scope != Scopes.COMMENT && scope != Scopes.STRING) {
-                scopeBefore = scope;
-                scope = Scopes.COMMENT;
-                continue;
-            }
-            
-            if (scope == Scopes.TOP_LEVEL) {
-                // Expecting operator declaration or proof object
-                skipWhiteSpace = true;
-                
-                if (c == '(') {
-                    scope = Scopes.OP_EXPECTED;
-                    readUntilWhiteSpace = true;
-                }
-            }
-            
-            System.out.print(c);
-            
-        }
-        
-        return result;
+        return "";
     }
 }
